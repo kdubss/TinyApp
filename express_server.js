0ce24b6a9279d654c84d046bf94d8aad6c76d99c
Kang Wang <kwang.inbox@gmail.com>
2017-11-20 22:29:16 -0800

    Initial/default Express server.

---
+++ express_server.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 8080 // default
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.listen(PORT () => {
+  console.log(`\nListening on PORT: ${PORT}\n`);
+})
