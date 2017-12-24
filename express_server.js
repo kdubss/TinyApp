1dc46d18a28d636767d7403eab9c5b4135f442ee
Kang Wang <kwang.inbox@gmail.com>
2017-12-23 23:09:55 -0500

    TODO for urls_show complete

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-let express = require('express');
-let app = express();
-let PORT = process.env.PORT || 8080 // default
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080 // default
 
 app.set('view engine', 'ejs');
 
@@ -14,14 +14,34 @@ app.get('/', (req, res) => {
 });
 app.get('/hello', (req, res) => {
   res.send(`\nURL Database: ${urlDatabase}`);
-})
+});
+
 // TODO: Add new route handler ('/urls') and use res.render() to pass url
 // data to the url_index.ejs template.
 app.get('/urls', (req, res) => {
+  console.log('\nWe\'re about to render the EJS page!\n');
   res.render('urls_index', {
-    urls: urlDatabase
+    urls: urlDatabase,
+    shortURL: req.params.id
   });
-})
+});
+
+// TODO: Now you're going to add another page,
+// this one for displaying a single URL and its shortened form.
+// The end point for such a page will be in the format /urls/b2xVn2,
+// which in this example means it's the page for the URL with the shortened
+// form (or ID) b2xVn2.
+// TODO: Create a new template file inside the views directory called
+// urls_show.ejs.
+// TODO: Fill out the urls_show.ejs template to display the following:
+// The full URL and its shortened form.
+app.get('/urls/:id', (req, res) => {
+  const shortURL = req.params.id
+  res.render('urls_show', {
+    urls: urlDatabase,
+    shortURL: shortURL
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`\nListening on PORT: ${PORT}\n`)
