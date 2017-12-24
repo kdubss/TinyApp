75d419cd9c23bfd8d973e812c1c385c7d3fd319d
Kang Wang <kwang.inbox@gmail.com>
2017-12-23 22:21:06 -0500

    get method for urls_index & urls_index EJS setup

--- express_server.js
+++ express_server.js
@@ -1,19 +1,28 @@
-let express = require("express");
+let express = require('express');
 let app = express();
 let PORT = process.env.PORT || 8080 // default
- 
+
+app.set('view engine', 'ejs');
+
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': "http://www.lighthouselabs.ca",
+  '9sm5xK': "http://www.google.com"
 }
- 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
-app.get("/hello", (req, res) => {
-  res.end(`\nURL Database:\n${urlDatabase}`);
+app.get('/hello', (req, res) => {
+  res.send(`\nURL Database: ${urlDatabase}`);
 })
- 
+// TODO: Add new route handler ('/urls') and use res.render() to pass url
+// data to the url_index.ejs template.
+app.get('/urls', (req, res) => {
+  res.render('urls_index', {
+    urls: urlDatabase
+  });
+})
+
 app.listen(PORT, () => {
   console.log(`\nListening on PORT: ${PORT}\n`)
 });
